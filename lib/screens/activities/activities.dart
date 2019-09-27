7ab013891fdfb214fa7a75faadfd78375290ca95
infinitium <kim.testa@curiouslab.sg>
2019-09-27 18:10:49 +0800

    Update

--- lib/screens/activities/activities.dart
+++ lib/screens/activities/activities.dart
@@ -1,3 +1,5 @@
+import 'package:contxt/screens/calculator/calculator.dart';
+import 'package:contxt/screens/todo/todo.dart';
 import 'package:flutter/material.dart';
 
 class ActivitiesScreen extends StatefulWidget {
@@ -23,6 +25,45 @@ class _ActivitiesScreenState extends State<ActivitiesScreen>
 
   @override
   Widget build(BuildContext context) {
+    Map _items = {
+      CalculatorScreen() : "Calculator",
+      TodoScreen() : "To Do"
+    };
+
+    Widget _listCards(Widget _widget, String _title, int _itemNum) {
+      return Card(
+        child: FlatButton(
+        padding: EdgeInsets.symmetric(horizontal: 0),
+          child: Container(
+            child: Row(
+              children: <Widget>[
+                SizedBox(
+                  child: Center(
+                    child: Text(_itemNum.toString() + ".", style: TextStyle(
+                      fontSize: 15.0,
+                    )),
+                  ),
+                  height: 55.0,
+                  width: 55.0,
+                ),
+                Expanded(
+                  child: Center(
+                    child: Text("Title")
+                  ),
+                ),
+                SizedBox(
+                  child: Icon(Icons.chevron_right),
+                  height: 55.0,
+                  width: 55.0
+                )
+              ],
+            ),
+          ),
+          onPressed: () {},
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("List of Activities"),
@@ -35,22 +76,9 @@ class _ActivitiesScreenState extends State<ActivitiesScreen>
           physics: ScrollPhysics(parent: BouncingScrollPhysics()),
           child: Column(
             children: <Widget>[
-              FlatButton(
-                child: Card(
-                  child: Container(
-                    child: Row(
-                      children: <Widget>[
-                        SizedBox(
-                          child: Icon(Icons.arrow_right),
-                          height: 55.0,
-                          width: 55.0,
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                onPressed: () {},
-              )
+              _items.forEach((k,v) => {
+
+              }),
             ],
           ),
         ),
